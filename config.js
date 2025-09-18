02dabac8d1b62535d8b8f85a72e426a6f0271bc2
FFAI888 <453870150@qq.com>
2025-09-18 17:34:46 +0800

    Update and rename config.js to login.html

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js - v0.40
-
-const NETWORK_ID_BSC = 56; // BSC Mainnet Chain ID
-const STORAGE_KEY_WALLET_ADDRESS = 'walletAddress';
-const STORAGE_KEY_INVITER_ADDRESS = 'inviterAddress';
-
-export { NETWORK_ID_BSC, STORAGE_KEY_WALLET_ADDRESS, STORAGE_KEY_INVITER_ADDRESS };
